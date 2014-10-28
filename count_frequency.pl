a56467ac944c9a55d85a6bb8436c680a2db20794
Terrence Wong <tw@andrew.cmu.edu>
2014-10-27 21:05:53 -0400

    add more codings and a new search mode: search for exact phrase by page and return a list of matching page numbers

--- count_frequency.pl
+++ count_frequency.pl
@@ -15,27 +15,49 @@ my $where_clause = "";
 my $display_string = "";
 my $current_group = 1;
 my %papers_included = ();
+my $raw_data_global = "";
 foreach my $raw_data (@raw_datas) {
   chomp $raw_data;
+  $raw_data_global = $raw_data;
   (my $this_group, my $this_technique, my $phrase) = split(/,/,$raw_data);
   if ($this_group != $current_group) {
-
-    print "result for group $current_group: " . (scalar keys %papers_included) . "\n";
-    print "------------------\n";
-
+    &print_result($raw_data_global, \%papers_included);
     $current_group = $this_group;
     %papers_included = ();
   }
 
-  if ($this_technique eq "exact") {
-    &exact_phrase_per_doc_search($phrase);
+  if ($this_technique =~ /exact(\d+)/) {
+    my $count_at_least = $1;
+    &exact_phrase_per_page_search($phrase, $count_at_least);
+  }
+  elsif ($this_technique eq "exact") {
+    &exact_phrase_per_doc_search($phrase, 1);
   }
   elsif ($this_technique eq "any") {
     &any_order_per_page_search($phrase);
   }
 }
-print "result for group $current_group: " . (scalar keys %papers_included) . "\n";
-print "------------------\n";
+&print_result($raw_data_global, \%papers_included);
+
+
+sub print_result($$) {
+  (my $raw_data_global, my $papers_included_ref) = @_;
+  my %papers_included = %{$papers_included_ref};
+
+  print "result for group $current_group ($raw_data_global): " . (scalar (keys %papers_included)) . " docs: \n";
+  foreach my $paper_name (sort keys %papers_included) {
+    print $papers_included{$paper_name}{"times"} . " times : " . $paper_name;
+    if (defined $papers_included{$paper_name}{"pages"}) {
+      print " (pages: ";
+      foreach my $page_num (@{$papers_included{$paper_name}{"pages"}}) {
+	print $page_num . " ";
+      }
+      print ")";
+    }
+    print "\n";
+  }
+  print "------------------\n";
+}
 
 sub any_order_per_page_search($) {
   (my $phrase) = @_;
@@ -47,27 +69,71 @@ sub any_order_per_page_search($) {
   }
   $where_clause =~ s/AND $//;
 
-  my $sql = "SELECT paper_id FROM pages WHERE preferred=1 AND $where_clause GROUP BY paper_id";
+  my $sql = "SELECT paper_id, name FROM pages, papers WHERE papers.id = pages.paper_id AND preferred=1 AND $where_clause GROUP BY paper_id";
   my $sth = $dbh->prepare($sql);
   $sth->execute
     or die "SQL Error: $DBI::errstr\n";
   while (my $row = $sth->fetchrow_hashref) {
-    $papers_included{$row->{'paper_id'}} = 1;
+    if (defined $papers_included{$row->{'name'}}) {
+      $papers_included{$row->{'name'}}{"times"}++;
+	}
+    else {
+      $papers_included{$row->{'name'}}{"times"} = 1;
+    }
   }
 
   $sth->finish();
 }
 
-sub exact_phrase_per_doc_search($) {
-  (my $phrase) = @_;
+#
+# @in count The number of times this phrase must appear in the full document before it is included 
+sub exact_phrase_per_doc_search($$) {
+  (my $phrase, my $count_at_least) = @_;
 
-  my $sql = "SELECT paper_id FROM papers_fulltext WHERE entiretext LIKE " . $dbh->quote("%".$phrase."%");
+  my $sql = "SELECT paper_id, name, entiretext FROM papers_fulltext, papers WHERE papers.id = papers_fulltext.paper_id AND entiretext LIKE " . $dbh->quote("%".$phrase."%");
   my $sth = $dbh->prepare($sql);
   $sth->execute
     or die "SQL Error: $DBI::errstr\n";
   while (my $row = $sth->fetchrow_hashref) {
-    $papers_included{$row->{'paper_id'}} = 1;
+    my $times_seen = () = $row->{'entiretext'} =~ /$phrase/g;
+    # yes, if $count_at_least is 1 we're duplicating work but it makes the code simpler
+    if ($times_seen >= $count_at_least) {
+      $papers_included{$row->{'name'}}{"times"} = $times_seen;
+    }
   }
 
+  $sth->finish();
+}
+
+#
+# @in count The number of times this phrase must appear in the full document before it is included 
+sub exact_phrase_per_page_search($) {
+  (my $phrase, my $count_at_least) = @_;
+
+  my $sql = "SELECT id, name FROM papers";
+  my $sth = $dbh->prepare($sql);
+  $sth->execute
+    or die "SQL Error: $DBI::errstr\n";
+  while (my $row = $sth->fetchrow_hashref) {
+    my $sql2 = "SELECT page_number, text FROM pages WHERE paper_id = " . $row->{'id'} . " AND preferred=1 AND text LIKE ". $dbh->quote("%".$phrase."%");
+    my $sth2 = $dbh->prepare($sql2);
+    $sth2->execute
+      or die "SQL Error: $DBI::errstr\n";
+    my $times_seen = 0;
+    my @page_list = ();
+    while (my $row2 = $sth2->fetchrow_hashref) {
+      my $times_this_page = () = $row2->{'text'} =~ /$phrase/g;
+      $times_seen += $times_this_page;
+      if ($times_this_page > 0) {
+	push @page_list, $row2->{'page_number'};
+      }
+
+      if ($times_seen >= $count_at_least) {
+	$papers_included{$row->{'name'}}{"times"} = $times_seen;
+	$papers_included{$row->{'name'}}{"pages"} = \@page_list;
+      }
+    }
+    $sth2->finish();
+  }
   $sth->finish();
 }
