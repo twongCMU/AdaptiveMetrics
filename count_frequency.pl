9e805eee1297c14dd4a08983918e8dd83a4d67cc
Terrence Wong <tw@andrew.cmu.edu>
2014-12-08 21:28:15 -0500

    add paper metrics: number of pages and number of words

--- count_frequency.pl
+++ count_frequency.pl
@@ -16,17 +16,21 @@ my $display_string = "";
 my $current_group = 1;
 my %papers_included = ();
 my $raw_data_global = "";
+my %grid_data = ();
+my $phrase_global;
 foreach my $raw_data (@raw_datas) {
   chomp $raw_data;
 
   (my $this_group, my $this_technique, my $phrase) = split(/,/,$raw_data);
   if ($this_group != $current_group) {
-    &print_result($raw_data_global, \%papers_included);
+    #&print_result($raw_data_global, \%papers_included);
+    $grid_data{$phrase_global} = &cloneHash();
     $current_group = $this_group;
     %papers_included = ();
   }
 
   $raw_data_global = $raw_data;
+  $phrase_global = $phrase;
 
   if ($this_technique =~ /exact(\d+)/) {
     my $count_at_least = $1;
@@ -39,8 +43,60 @@ foreach my $raw_data (@raw_datas) {
     &any_order_per_page_search($phrase);
   }
 }
-&print_result($raw_data_global, \%papers_included);
+#&print_result($raw_data_global, \%papers_included);
+$grid_data{$phrase_global} = &cloneHash();
 
+print "X;";
+my $one_grid_column;
+foreach my $grid_column (sort keys %grid_data) {
+  print $grid_column . ";";
+  $one_grid_column = $grid_column;
+}
+print "\n";
+
+my %foo = &getListOfPapers();
+# list of papers
+foreach my $grid_row (sort keys %foo) {
+  print $grid_row . "; ";
+  foreach my $grid_column (sort keys %grid_data) {# list of states
+#    print STDERR $grid_data{$grid_column}{$grid_row}{"times"} . ", $grid_column, $grid_row\n";
+    if (!(defined $grid_data{$grid_column}{$grid_row}{"times"})) {
+      print "0;";
+    }
+    else {
+      print $grid_data{$grid_column}{$grid_row}{"times"} . ";";
+    }
+  }
+  print "\n";
+}
+	
+#get metrics doc title, #words in doc, # pages in doc
+# list of papers
+print "==== START PAGE METRICS ===\n";
+foreach my $grid_row (sort keys %foo) {	    
+  &paper_stats($foo{$grid_row}, $grid_row);
+}
+
+sub paper_stats($$) {
+  (my $paper_id, my $paper_name) = @_;
+
+  my $sql = "SELECT COUNT(*) as page_count FROM pages WHERE paper_id=$paper_id AND preferred=1";
+  my $sth = $dbh->prepare($sql);
+  $sth->execute
+    or die "SQL Error: $DBI::errstr\n";
+  my $row = $sth->fetchrow_hashref;
+  my $pages = $row->{"page_count"};
+  $sth->finish();
+
+  $sql = "SELECT entiretext FROM papers_fulltext WHERE paper_id=$paper_id";
+  $sth = $dbh->prepare($sql);
+  $sth->execute
+    or die "SQL Error: $DBI::errstr\n";
+   $row = $sth->fetchrow_hashref;
+  my @words = split(/\s+/, $row->{"entiretext"});
+  print "$paper_name ; $pages ; " . (scalar @words) . "\n";
+  $sth->finish();
+}
 
 sub print_result($$) {
   (my $raw_data_global, my $papers_included_ref) = @_;
@@ -99,12 +155,26 @@ sub exact_phrase_per_doc_search($$) {
   while (my $row = $sth->fetchrow_hashref) {
     my $times_seen = () = $row->{'entiretext'} =~ /$phrase/g;
     # yes, if $count_at_least is 1 we're duplicating work but it makes the code simpler
-    if ($times_seen >= $count_at_least) {
+    #if ($times_seen >= $count_at_least) {
       $papers_included{$row->{'name'}}{"times"} = $times_seen;
-    }
+    #}
   }
 
   $sth->finish();
+}
+
+sub getListOfPapers() {
+  my %foo = ();
+  
+  my $sql = "SELECT id, name FROM papers";
+  my $sth = $dbh->prepare($sql);
+  $sth->execute
+    or die "SQL Error: $DBI::errstr\n";
+  while (my $row = $sth->fetchrow_hashref) {
+    $foo{$row->{"name"}} = $row->{"id"};
+  }
+  $sth->finish();
+  return %foo;
 }
 
 #
@@ -129,13 +199,22 @@ sub exact_phrase_per_page_search($) {
       if ($times_this_page > 0) {
 	push @page_list, $row2->{'page_number'};
       }
-
-      if ($times_seen >= $count_at_least) {
-	$papers_included{$row->{'name'}}{"times"} = $times_seen;
-	$papers_included{$row->{'name'}}{"pages"} = \@page_list;
-      }
     }
     $sth2->finish();
+    # if ($times_seen >= $count_at_least) {
+    #print STDERR "saving times_seen $times_seen for $phrase, $row->{'name'}\n";
+    $papers_included{$row->{'name'}}{"times"} = $times_seen;
+    $papers_included{$row->{'name'}}{"pages"} = \@page_list;
+    # }
   }
   $sth->finish();
+}
+
+sub cloneHash() {
+  my %new_hash = ();
+  foreach my $key (keys %papers_included) {
+    $new_hash{$key}{"times"} = $papers_included{$key}{"times"};
+    $new_hash{$key}{"pages"} = $papers_included{$key}{"pages"};
+  }
+  return \%new_hash;
 }
